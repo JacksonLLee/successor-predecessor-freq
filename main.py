ab65497cf1afb8d020fe5f7ba16fbab75859f32a
Jackson L. Lee <jsllee@uchicago.edu>
2014-07-26 05:37:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# Modeling Brazilian Portuguese truncation by successor and predecessor frequencies
+# Modeling Brazilian Portuguese truncation using successor frequencies
 # Mike Pham, Jackson Lee, and Bruna Moreira
-#
-# citation:
-# Pham, Mike, Jackson L. Lee, and Bruna da Costa Moreira. 2014.
-# "Modeling Brazilian Portuguese truncation by successor and predecessor frequencies".
-# Midwest Speech and Language Days 2014, University of Illinois at Urbana-Champaign. May 2-3, 2014.
 
 import math
 import string
@@ -17,6 +12,18 @@ def printTerminalTxt(s, f):
 	print s
 	f.write(s + '\n')
 
+def elbowPoint(points):
+	'''takes a list of points and outputs the index of the list for maximal curvature'''
+	secondDerivativeList = [points[i+1] + points[i-1] - 2*points[i] for i in range(1, len(points) - 1)]
+	secondDerivativeListPointTuplesSorted = sorted(enumerate(secondDerivativeList), key=lambda x:x[1], reverse=True)
+	return secondDerivativeListPointTuplesSorted[0][0] + 2
+
+def intersection(points1, points2):
+	for (idx, (p1, p2)) in enumerate(zip(points1, points2)):
+		if p2 > p1:
+			return idx+1
+
+
 #######################################################################
 ###	reads in lexicon, input full forms, and outputs numbers			###
 #######################################################################
@@ -24,8 +31,12 @@ def printTerminalTxt(s, f):
 lexicon = open('pt_br.txt').read().lower().split('\r\n')
 # lexicon = open('Ncorpus.txt').read().lower().split('\n')
 #testwords = open('input.txt').read().lower().split('\n')
-testwords = [x[:x.index('\t')].replace('\n','').replace('|','') for x in open('goldStandard.txt')]
-goldStandard = [x[:x.index('\t')].replace('\n','') for x in open('goldStandard.txt')]
+#testwords = [x[:x.index('\t')].replace('\n','').replace('|','') for x in open('goldStandard.txt')]
+#goldStandard = [x[:x.index('\t')].replace('\n','') for x in open('goldStandard.txt')]
+
+#expanded gold standard list
+testwords = [x[:x.index('\t')].replace('\n','').replace('|','') for x in open('goldStandard_MP.txt')]
+goldStandard = [x[:x.index('\t')].replace('\n','') for x in open('goldStandard_MP.txt')]
 
 output = open('output.txt', 'w')
 
@@ -46,24 +57,35 @@ outTex.write('\\usepackage[letterpaper, margin=.5in]{geometry}\n')
 outTex.write('\\setlength{\\parindent}{0em}\n')
 outTex.write('\\begin{document}\n')
 
+
+SFpredictList = list()
+PFpredictList = list()
+SFPFpredictList = list()
+truncPointList = list()
+
+
 ###############################################################################
 ###	This builds up the full form incrementally from the first letter,		###
 ###	checking each time to see how many words in the lexicon can be formed	###
 ### from that truncated form												###
 ###############################################################################
 for (fullform, gold) in zip(testwords, goldStandard):
+	print fullform
+	truncPoint = gold.index('|')
+	truncPointList.append(truncPoint)
 	fullform = fullform.lower()
 	fullformReversed = fullform[::-1]
 	trunc = ''
 	truncReversed = ''
-	truncdisplay = 'trunc:' + '\t\t'
-	perletterdisplay = 'suc-freq:' + '\t'
-	logcountdisplay = 'log(SF):' + '\t'
-#	perletterPFdisplay = 'pred-freq:' + '\t' # for predecessor freq
-#	logcountPFdisplay = 'log(PF):' + '\t' # for predecessor freq
-	perletterPFdisplay = '' # for predecessor freq
-	logcountPFdisplay = '' # for predecessor freq
-	truncrankdisplay = 'lex-freq:' + '\t'
+
+#	truncdisplay = 'trunc:' + '\t\t'
+#	perletterdisplay = 'suc-freq:' + '\t'
+#	logcountdisplay = 'log(SF):' + '\t'
+##	perletterPFdisplay = 'pred-freq:' + '\t' # for predecessor freq
+##	logcountPFdisplay = 'log(PF):' + '\t' # for predecessor freq
+#	perletterPFdisplay = '' # for predecessor freq
+#	logcountPFdisplay = '' # for predecessor freq
+#	truncrankdisplay = 'lex-freq:' + '\t'
 
 	truncdisplayTex = 'trunc:' + ' & '
 	perletterdisplayTex = 'suc-freq:' + ' & '
@@ -83,10 +105,13 @@ for (fullform, gold) in zip(testwords, goldStandard):
 	except KeyError:
 		continue
 
-	printTerminalTxt('----------' + '\n' + string.lower(fullform), output)
+#	printTerminalTxt('----------' + '\n' + string.lower(fullform), output)
 	outTex.write('%s\n\n%s\n\n\\vspace{1em}\n\n' % (fullform, gold.replace('|','$|$')))
 	outTex.write('\\begin{tabular}{l|%s}\n\n' % ('l'*len(fullform)))
 	goldPosition = gold.index('|')
+
+	logcountList = list()
+	logcountReversedList = list()
 
 	for (e, (letter, letterReversed)) in enumerate(zip(fullform, fullformReversed)):
 		count = 0
@@ -95,7 +120,7 @@ for (fullform, gold) in zip(testwords, goldStandard):
 		trunc = trunc + letter
 		truncReversed = truncReversed + letterReversed
 
-		truncdisplay = truncdisplay + letter + '\t'
+#		truncdisplay = truncdisplay + letter + '\t'
 		if e < goldPosition:
 			letterTex = '{\\color{red}\\bf ' + letter + '}'
 		else:
@@ -137,14 +162,17 @@ for (fullform, gold) in zip(testwords, goldStandard):
 		else:
 			logcountReversed = 0
 
+		logcountList.append(logcount)
+		logcountReversedList.append(logcountReversed)
+
 		######################################
 		##	Builds nicer display of numbers	##
 		######################################
-		perletterdisplay = perletterdisplay + str(count) + '\t'
-		logcountdisplay = logcountdisplay + str(logcount) + '\t'
-		perletterPFdisplay = '\t' + str(countReversed) + perletterPFdisplay
-		logcountPFdisplay =  '\t' + str(logcountReversed) + logcountPFdisplay
-		truncrankdisplay = truncrankdisplay + str(truncrank) + '\t'
+#		perletterdisplay = perletterdisplay + str(count) + '\t'
+#		logcountdisplay = logcountdisplay + str(logcount) + '\t'
+#		perletterPFdisplay = '\t' + str(countReversed) + perletterPFdisplay
+#		logcountPFdisplay =  '\t' + str(logcountReversed) + logcountPFdisplay
+#		truncrankdisplay = truncrankdisplay + str(truncrank) + '\t'
 
 		perletterdisplayTex = perletterdisplayTex + str(count) + ' & '
 		logcountdisplayTex = logcountdisplayTex + str(logcount) + ' & '
@@ -152,13 +180,13 @@ for (fullform, gold) in zip(testwords, goldStandard):
 		logcountPFdisplayTex =  ' & ' + str(logcountReversed) + logcountPFdisplayTex
 		truncrankdisplayTex = truncrankdisplayTex + str(truncrank) + ' & '
 
-	printTerminalTxt(gold + '\n', output)
-	printTerminalTxt(truncdisplay, output)
-	printTerminalTxt(perletterdisplay, output)
-	printTerminalTxt(logcountdisplay, output)
-	printTerminalTxt('pred-freq:' + perletterPFdisplay, output)
-	printTerminalTxt('log(PF):' + logcountPFdisplay, output)
-	printTerminalTxt(truncrankdisplay, output)
+#	printTerminalTxt(gold + '\n', output)
+#	printTerminalTxt(truncdisplay, output)
+#	printTerminalTxt(perletterdisplay, output)
+#	printTerminalTxt(logcountdisplay, output)
+#	printTerminalTxt('pred-freq:' + perletterPFdisplay, output)
+#	printTerminalTxt('log(PF):' + logcountPFdisplay, output)
+#	printTerminalTxt(truncrankdisplay, output)
 
 	outTex.write(truncdisplayTex[:-3] + ' \\\\ \n')
 	outTex.write(perletterdisplayTex[:-3] + ' \\\\ \n')
@@ -168,8 +196,69 @@ for (fullform, gold) in zip(testwords, goldStandard):
 	outTex.write(truncrankdisplayTex[:-3] + ' \\\\ \n')
 
 	outTex.write('\\end{tabular}\n\n')
+
+	outTex.write('trunc point: ' + str(truncPoint) + '\n\n')
+
+	SFpredict = elbowPoint(logcountList)
+	PFpredict = elbowPoint(logcountReversedList)
+	SFPFpredict = intersection(logcountList, logcountReversedList[::-1])
+
+	SFpredictList.append(SFpredict)
+	PFpredictList.append(PFpredict)
+	SFPFpredictList.append(SFPFpredict)
+
+	outTex.write('SF point: ' + str(SFpredict)+'\n\n')
+	outTex.write('PF point: ' + str(PFpredict)+'\n\n')
+
+	outTex.write('SF+PF point: ' + str(SFPFpredict) + '\n\n')
+
 	outTex.write('\\vspace{3em}\n\n')
 
+#	print logcountList
+#	print logcountReversedList
+#	print
+
+##########################################################
+### evaluation
+##########################################################
+
+print
+
+evaluationList = list()
+
+SFevaluationList = list()
+PFevaluationList = list()
+SFPFevaluationList = list()
+
+printTerminalTxt('{0:15s} {1:10s} {2:10s} {3:10s} {4:10s} {5:12s} {6:12s}'.format('word',
+																				'SF-after', 'SF-before',
+																				'PF-after', 'PF-before',
+																				'SFPF-after', 'SFPF-before'), output)
+
+for (gold, T, SF, PF, SFPF) in zip(goldStandard, truncPointList, SFpredictList, PFpredictList, SFPFpredictList):
+	SFeval = T-SF
+	PFeval = T-PF
+	SFPFeval = T-SFPF
+
+	SFevaluationList.append(SFeval)
+	PFevaluationList.append(PFeval)
+	SFPFevaluationList.append(SFPFeval)
+
+	printTerminalTxt('{0:15s} {1:10d} {2:10d} {3:10d} {4:10d} {5:12d} {6:12d}'.format(gold,
+																				SFeval, SFeval+1,
+																				PFeval, PFeval+1,
+																				SFPFeval, SFPFeval+1), output)
+
+lenGold = len(goldStandard)
+
+printTerminalTxt('evaluation:', output)
+printTerminalTxt('SF-after SF-before PF-after PF-before SFPF-after SFPF-before\n{0} {1} {2} {3} {4} {5}'.format(sum(SFevaluationList), sum(SFevaluationList)+lenGold, 
+																			sum(PFevaluationList), sum(PFevaluationList)+lenGold,
+																			sum(SFPFevaluationList), sum(SFPFevaluationList)+lenGold), output)
+
+
+
+############################################################
 # close open file objects and compile .tex file
 
 outTex.write('\\end{document}\n')
